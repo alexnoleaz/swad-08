0d86e02d4fe7978a8eec0fbc5860814b7cd84d9a
Alexander Nole <cnole_azabache@outlook.com>
2025-05-14 12:49:26 -0500

    refactor: update models

--- src/main/java/com/example/swad8/models/Category.java
+++ src/main/java/com/example/swad8/models/Category.java
@@ -1,6 +1,7 @@
 package com.example.swad8.models;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
--- src/main/java/com/example/swad8/models/Product.java
+++ src/main/java/com/example/swad8/models/Product.java
@@ -1,12 +1,12 @@
 package com.example.swad8.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,7 +20,10 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private Double price;
 
     @ManyToOne
