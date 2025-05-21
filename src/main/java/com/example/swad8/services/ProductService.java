fa4cc156e82ee76cd6f71a5e644d2332dc12b024
Alexander Nole <cnole_azabache@outlook.com>
2025-05-21 18:25:16 -0500

    update product service

--- src/main/java/com/example/swad8/services/ProductService.java
+++ src/main/java/com/example/swad8/services/ProductService.java
@@ -10,6 +10,7 @@ import com.example.swad8.dtos.CreateProductDto;
 import com.example.swad8.dtos.ProductDto;
 import com.example.swad8.dtos.UpdateProductDto;
 import com.example.swad8.mappers.ProductMapper;
+import com.example.swad8.models.Product;
 import com.example.swad8.repositories.CategoryRepository;
 import com.example.swad8.repositories.ProductRepository;
 
@@ -17,6 +18,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 
+@Transactional
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
@@ -36,20 +38,31 @@ public class ProductService {
     }
 
     public List<ProductDto> findAll() {
-        return productRepository.findAll()
+        // return productRepository.findAll()
+        // .stream()
+        // .map(productMapper::toDto)
+        // .collect(Collectors.toList());
+        return entityManager.createQuery("SELECT p FROM Product p", Product.class).getResultList()
                 .stream()
                 .map(productMapper::toDto)
                 .collect(Collectors.toList());
     }
 
     public ProductDto findById(Long id) {
-        return productMapper.toDto(
-                productRepository
-                        .findById(id)
-                        .orElseThrow(() -> new NoSuchElementException("Product with id " + id + " not found")));
+        // return productMapper.toDto(
+        // productRepository
+        // .findById(id)
+        // .orElseThrow(() -> new NoSuchElementException("Product with id " + id + " not
+        // found")));
+        var dbProduct = entityManager.createQuery("SELECT p FROM Product p WHERE p.id = :id", Product.class)
+                .setParameter("id", id)
+                .getResultStream()
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Product with id " + id + " not found"));
+
+        return productMapper.toDto(dbProduct);
     }
 
-    @Transactional
     public ProductDto create(CreateProductDto input) {
         var dbCategory = categoryRepository.findById(input.getCategoryId()).orElseThrow(
                 () -> new NoSuchElementException("Category with id " + input.getCategoryId() + " not found"));
@@ -61,18 +74,31 @@ public class ProductService {
         return productMapper.toDto(product);
     }
 
-    @Transactional
     public ProductDto update(UpdateProductDto input) {
-        var dbProduct = productRepository.findById(input.getId())
+        // var dbProduct = productRepository.findById(input.getId())
+        // .orElseThrow(() -> new NoSuchElementException("Product with id " +
+        // input.getId() + " not found"));
+
+        // productMapper.updateProductFromDto(input, dbProduct);
+        // entityManager.persist(dbProduct);
+
+        // return productMapper.toDto(dbProduct);
+
+        var dbProduct = entityManager.createQuery("SELECT p FROM Product p WHERE p.id = :id", Product.class)
+                .setParameter("id", input.getId())
+                .getResultStream()
+                .findFirst()
                 .orElseThrow(() -> new NoSuchElementException("Product with id " + input.getId() + " not found"));
 
         productMapper.updateProductFromDto(input, dbProduct);
-        entityManager.persist(dbProduct);
-
         return productMapper.toDto(dbProduct);
     }
 
     public void delete(Long id) {
-        productRepository.deleteById(id);
+        // productRepository.deleteById(id);
+
+        entityManager.createQuery("DELETE FROM Product p WHERE p.id = :id")
+                .setParameter("id", id)
+                .executeUpdate();
     }
 }
